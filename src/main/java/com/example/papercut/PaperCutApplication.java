5092baf7ee32172001cdc81840d89a618935ce64
Smile_简柠 <2646392644@qq.com>
2024-03-16 19:06:16 +0800

    add CORS

--- src/main/java/com/example/papercut/PaperCutApplication.java
+++ src/main/java/com/example/papercut/PaperCutApplication.java
@@ -2,6 +2,7 @@ package com.example.papercut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 public class PaperCutApplication {
